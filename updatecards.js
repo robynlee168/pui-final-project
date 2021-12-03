0b6cc42817b7c55a992c635d4209eada4b286905
robynlee168 <91481505+robynlee168@users.noreply.github.com>
2021-12-03 15:56:23 -0500

    spring constellation info cards

--- updatecards.js
+++ updatecards.js
@@ -1,10 +1,10 @@
-function updateGallery() {
+function updateCards() {
     var season = document.getElementById("season-select");
     var selectedValue = season.options[season.selectedIndex].value;
     var thumbnails = document.querySelectorAll(".spring, .summer, .fall, .winter");
     
-    var row = document.querySelector('.row');
-    row.style.display = "unset";
+    // var row = document.querySelector('.row');
+    // row.style.display = "unset";
 
     for (let i = 0; i < thumbnails.length; i++) {
         if ((selectedValue == "none") || (!thumbnails[i].classList.contains(selectedValue)))
